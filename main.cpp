d75824df2c4b5659472919ed0b0dd1160a22d60c
Calin8421 <gheorghecalin169@gmail.com>
2025-10-29 22:39:44 +0200

    CRUD function header bis

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+
+int DELETE()
+{
+	return 0;
+}
+int DISPLAY()
+{
+	return 0;
+}
+int SELECT()
+{
+	return 0;
+}
+int DROP()
+{
+	return 0;
+}
+int INSERT()
+{
+	return 0;
+}
+int UPDATE()
+{
+	return 0;
+}
+int CREATE()
+{
+
+	return 0;
+}
+
+int commander()
+{
+
+}
+
+void main()
+{
+	//std::cout << "SQL Engine here";
+
+
+}
